57edeaf7bbf728b8d5dec67a085a05d5b106a2bc
Parham Heidari <heidari1377@gmail.com>
2021-10-21 22:44:19 +0330

    remove extra styles and apply some default options to calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import TUICalendar from "@toast-ui/react-calendar";
+import { useState } from "react";
+
+import "tui-calendar/dist/tui-calendar.css";
+import "tui-date-picker/dist/tui-date-picker.css";
+import "tui-time-picker/dist/tui-time-picker.css";
 import './App.css';
 
 function App() {
+  const [schedules, setSchedules] = useState([]);
+
+  const updateSchedule = (start, end) => {
+
+    const newScheduleItem = {
+      calendarId: "1",
+      category: "time",
+      isVisible: true,
+      isReadOnly:false,
+      title: "Parham",
+      id: "1",
+      body: "Test",
+      start,
+      end,
+    }
+    setSchedules([...schedules, newScheduleItem])
+  }
+  console.log(`schedules`, schedules)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TUICalendar
+        // ref={cal}
+        calendars={{}}
+        usageStatistics={false}
+        height="100%"
+        view="week"
+        taskView={false}
+        scheduleView={['time']} 
+        week={{startDayOfWeek:6, 
+        daynames:[
+          'یکشنبه',
+          'دوشنبه',
+          'سه شنبه',
+          'چهار شنبه',
+          'پنج شنبه',
+          'جمعه',
+          'شنبه',
+        ],
+        
+      }}
+        
+        // template={templates}
+        // calendars={calendars}
+        schedules={[...schedules]}
+        onClickSchedule={e=>console.log(`e`, e)}
+        onBeforeCreateSchedule={e=>updateSchedule(e.start, e.end)}
+        // onBeforeDeleteSchedule={onBeforeDeleteSchedule}
+        // onBeforeUpdateSchedule={onBeforeUpdateSchedule}
+      />
+    
     </div>
   );
 }
